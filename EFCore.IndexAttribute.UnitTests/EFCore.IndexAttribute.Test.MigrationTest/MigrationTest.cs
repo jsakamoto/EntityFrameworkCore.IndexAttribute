6a501136595382d992526e514afd3f36a9f67aec
jsakamoto <junichi.jp@gmail.com>
2022-03-29 14:13:12 -0700

    Modernize all unit tests

--- EFCore.IndexAttribute.UnitTests/EFCore.IndexAttribute.Test.MigrationTest/MigrationTest.cs
+++ EFCore.IndexAttribute.UnitTests/EFCore.IndexAttribute.Test.MigrationTest/MigrationTest.cs
@@ -1,48 +1,27 @@
-using System;
-using System.IO;
-using Xunit;
-using static EFCore.IndexAttribute.Test.MigrationTest.Internals.Shell;
+using Toolbelt;
+using static Toolbelt.Diagnostics.XProcess;
 
-namespace EFCore.IndexAttribute.Test.MigrationTest
+namespace EFCore.IndexAttribute.Test.MigrationTest;
+
+public class MigrationTest
 {
-    public class MigrationTest : IDisposable
+    [Test]
+    public async Task AddMigration_Initial_Test()
     {
-        private string TestProjDir { get; }
+        var testDir = FileIO.FindContainerDirToAncestor("*.csproj");
+        using var workDir = WorkDirectory.CreateCopyFrom(Path.Combine(testDir, "TestProject"), _ => true);
 
-        public MigrationTest()
-        {
-            var testProjSrcDir = Path.Combine(GetTestDir(), "TestProject");
-            TestProjDir = Environment.ExpandEnvironmentVariables(Path.Combine("%TEMP%", Guid.NewGuid().ToString("N")));
-            Directory.CreateDirectory(TestProjDir);
-            Run(testProjSrcDir, "xcopy", "/S",
-                $"\"{Path.Combine(testProjSrcDir, "*.*")}\"",
-                $"\"{Path.Combine(TestProjDir, "*.*")}\"");
-        }
+        // Check the project is fine for build before adding migration.
+        var build = await Start("dotnet", "build --nologo", workDir).WaitForExitAsync();
+        build.ExitCode.Is(0, message: build.Output);
 
-        [Fact]
-        public void AddMigration_Initial_Test()
-        {
-            // Check the project is fine for build before adding migration.
-            Run(TestProjDir, "dotnet", "build").ExitCode.Is(0);
+        // Add migration code, and...
+        //Run(this.TestProjDir, "dotnet", "ef", "migrations", "add", "initial").ExitCode.Is(0);
+        var migration = await Start("dotnet", "ef migrations add initial", workDir).WaitForExitAsync();
+        migration.ExitCode.Is(0, message: build.Output);
 
-            // Add migration code, and...
-            Run(TestProjDir, "dotnet", "ef", "migrations", "add", "initial").ExitCode.Is(0);
-
-            // Build it, and check it will be succeeded as expectedly.
-            Run(TestProjDir, "dotnet", "build").ExitCode.Is(0);
-        }
-
-        private static string GetTestDir()
-        {
-            var testDir = AppDomain.CurrentDomain.BaseDirectory;
-            do { testDir = Path.GetDirectoryName(testDir); } while (!Exists(testDir, "*.sln"));
-            testDir = Path.Combine(testDir, "EFCore.IndexAttribute.UnitTests", "EFCore.IndexAttribute.Test.MigrationTest");
-            return testDir;
-        }
-
-        public void Dispose()
-        {
-            try { Delete(TestProjDir); } catch { }
-        }
+        // Build it, and check it will be succeeded as expectedly.
+        var rebuild = await Start("dotnet", "build --nologo", workDir).WaitForExitAsync();
+        rebuild.ExitCode.Is(0, message: build.Output);
     }
 }
